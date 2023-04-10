2d6121b7be85cc8fafbcbb4b43c9fc1912349c1a
Naoki08 <wnp1998@gmail.com>
2023-04-10 13:56:22 +0900

    apiサーバーを立てた

--- node/pages/api/generate.js
+++ node/pages/api/generate.js
@@ -15,23 +15,39 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const inputText = req.body.inputText || '';
+  if (inputText.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "食材を入力してください",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.chatCreateCompletion({
+    const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      prompt: generatePrompt(animal),
+      messages: [
+        {
+          role: "system",
+          content: chatConfig
+        },
+        {
+          role: "user",
+          content: `
+            食材: {
+              ジャガイモ: 5,
+              にんじん: 4,
+              バター: 5
+            },
+            手間: 50
+          `
+        }
+      ],
       temperature: 0.6,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: completion.data.choices[0].message.content });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -48,15 +64,30 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+const chatConfig = `
+あなたにはこれから以下の2つの人格を演じてもらいます。
+    1つ目は、「プロの料理人」です。
+    2つ目は、「プロの料理批評家」です。
+    以下に、それぞれの性質を示します。
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+    # プロの料理人の性質
+    - 指定された{食材}に対して、それがどんな食材であっても、それに応じたレシピを考えることができます。
+    - レシピを、順序立てて説明することができます。
+    - レシピは、家庭で調理できるように簡単な手順を用います。
+    - 入力された{手間}に応じて、レシピの複雑さを調節することができます。
+    - 手間は数値で表現され、0が最も簡単で、100が最も複雑です。
+    - 口調は丁寧です。
+
+    # プロの料理批評家の性質
+    - どんな料理に対しても、味を評価することができます。
+    - 正直者で、美味しいものには高い評価を、美味しくないものにはひどい評価をします。
+    - 口調はくだけていて、フレンドリーです。
+
+    以下に出力の形式を示します。出力は以下の形式に厳格に従ってください。
+
+    # 出力の形式
+    レシピ: {プロの料理人の考えたレシピ}
+    レビュー: {プロの料理批評家の、上記のレシピから作られる料理に対する批評}
+
+    それでは、JSON形式で{食材}と{手間}を入力するので、応答を生成してください。
+`
